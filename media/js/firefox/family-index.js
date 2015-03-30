f016706a9b12cf659be054aa88274b124bb42ec3
Craig Cook <craigcook@gmail.com>
2015-03-30 00:47:22 -0700

    Flexbox to the rescue!
    And other misc. tweaks

--- media/js/firefox/family-index.js
+++ media/js/firefox/family-index.js
@@ -2,9 +2,10 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-;(function($) {
+;(function($, _gaq, site) {
     'use strict';
 
+    var $html = $(document.documentElement);
     var $downloadbar = $('#conditional-download-bar');
 
     // Open external links in new window/tab
@@ -19,5 +20,18 @@
         });
     });
 
+    if (isFirefox()) {
+        // data-latest-firefox includes point release information
+        var latestFirefoxVersionFull = $html.attr('data-latest-firefox');
 
-})(window.jQuery);
+        // get latest full version (no point release info) for initial check
+        var latestFirefoxVersion = parseInt(latestFirefoxVersionFull.split('.')[0], 10);
+
+        if (isFirefoxUpToDate(latestFirefoxVersion + '')) {
+            $html.addClass('firefox-latest');
+        } else {
+            $html.addClass('firefox-old');
+        }
+    }
+
+})(window.jQuery, window._gaq, window.site);
